911b9f8cd313277c5118c3e08f7094d4a40dd7a0
Yakub Yusuf <yakubyusuf608@gmail.com>
2024-05-07 13:15:50 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,19 +5,19 @@ const Home = () => {
   return (
     <>
     <div className='py-10 mt-20'>
-     <section>
-      <div className='grid grid-cols-2'>
+     <section className='get'>
+      <div id='' className='grid grid-cols-2'>
         <div>
-        <h1 className='text-4xl'>GEAR UP AND DOMINATE YOUR <br /> ACTIVE LIFESTYLE WITH OUR <br /> PREMIUM SPORTWEAR</h1>
-        <p>shop the finest collection of sportwear and accessories at unbeatable prices</p>
-        <div className=''>
-          <button className='bg-red-300 '>Shop Women</button>
-          <button className='bg-red-300 ms-2'>Shop men</button>
+        <h1 id='h1' className='lg:text-4xl md:text-2xl  sm:text-2xl py-2 '>GEAR UP AND DOMINATE YOUR  ACTIVE LIFESTYLE WITH OUR PREMIUM SPORTWEAR</h1>
+        <p className='text-lg text-[#7d7d7d] p-1'>Shop the finest collection of sportwear and <br /> accessories at unbeatable prices</p>
+        <div className='flex p-2'>
+          <button className='bg-[#bf9046] text-white rounded text-xs p-5'>Shop Women</button>
+          <button className='ms-2  bg-[#bf9046] p-4 rounded text-white text-xs'>Shop men</button>
         </div>
         </div>
 
         <div>
-    <img src={shape} width={400} alt="" />
+    <img id='img' src={shape}  alt="" />
     </div>
       
       </div>
