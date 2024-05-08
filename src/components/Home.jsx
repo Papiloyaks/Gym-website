9270176341bd87b1cd2d38cb2504b61028525e02
Yakub Yusuf <yakubyusuf608@gmail.com>
2024-05-08 09:48:25 +0100

    commit

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,20 +4,20 @@ import shape from '../assets/Gym project/shape.png'
 const Home = () => {
   return (
     <>
-    <div className='py-10 mt-20'>
-     <section className='get'>
-      <div id='' className='grid grid-cols-2'>
+    <div>
+     <section className='get py-20 p-10'>
+      <div id='' className='lg:grid grid-cols-2 md:grid grid-cols-2 sm:grid grid-cols-2'>
         <div>
-        <h1 id='h1' className='lg:text-4xl md:text-2xl  sm:text-2xl py-2 '>GEAR UP AND DOMINATE YOUR  ACTIVE LIFESTYLE WITH OUR PREMIUM SPORTWEAR</h1>
-        <p className='text-lg text-[#7d7d7d] p-1'>Shop the finest collection of sportwear and <br /> accessories at unbeatable prices</p>
+        <h1 id='h1' className='lg:text-4xl md:text-2xl  sm:text-2xl py-2 font-semi-bold mt-20'>GEAR UP AND DOMINATE YOUR <br />  ACTIVE LIFESTYLE WITH OUR PREMIUM SPORTWEAR</h1>
+        <p className='lg:text-xl sm:text-xs md:text-xs text-[#7d7d7d] p-1'>Shop the finest collection of sportwear and accessories at unbeatable prices</p>
         <div className='flex p-2'>
           <button className='bg-[#bf9046] text-white rounded text-xs p-5'>Shop Women</button>
           <button className='ms-2  bg-[#bf9046] p-4 rounded text-white text-xs'>Shop men</button>
         </div>
         </div>
 
-        <div>
-    <img id='img' src={shape}  alt="" />
+        <div className=''>
+    <img className='mt-10' src={shape} width={500}  alt="" />
     </div>
       
       </div>
